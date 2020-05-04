1251bbceb8e14453512c8d4080e7dd07dc52cee5
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-05-04 09:17:48 -0700

    simplify rpm build script

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # usage:
-# build-rpm.sh <version>
-# 
-# e.g.:
-# build-rpm.sh 1.10.7
+# build-rpm.sh
 
-if [[ $1 == "" ]]; then
-    echo "Please enter the version to build."
-    exit
-fi
+# if [[ $1 == "" ]]; then
+#     echo "Please enter the version to build."
+#     exit
+# fi
 
-VERSION=$1
+# VERSION=$1
 
 # build the SRPM
 pyp2rpm jc --srpm
 
 # build the RPM
-rpmbuild --rebuild ~/rpmbuild/SRPMS/python-jc-"${VERSION}"-1.fc32.src.rpm
+# rpmbuild --rebuild ~/rpmbuild/SRPMS/python-jc-"${VERSION}"-1.fc32.src.rpm
+
+# build the RPM
+rpmbuild -bb ~/rpmbuild/SPEC/python-jc.spec 
