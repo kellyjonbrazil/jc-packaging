ec28d803ccf060938d64bf98c80d177f89ec621f
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-05-08 10:34:54 -0700

    working rpm build script

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# usage:
-# build-rpm.sh
+# old:
+    # usage:
+    # build-rpm.sh
 
-# if [[ $1 == "" ]]; then
-#     echo "Please enter the version to build."
-#     exit
-# fi
+    # if [[ $1 == "" ]]; then
+    #     echo "Please enter the version to build."
+    #     exit
+    # fi
 
-# VERSION=$1
+    # VERSION=$1
 
-# build the SRPM
-# pyp2rpm jc --srpm
+    # build the SRPM
+    # pyp2rpm jc --srpm
 
-# build the RPM
-# rpmbuild --rebuild ~/rpmbuild/SRPMS/python-jc-"${VERSION}"-1.fc32.src.rpm
+    # build the RPM
+    # rpmbuild --rebuild ~/rpmbuild/SRPMS/python-jc-"${VERSION}"-1.fc32.src.rpm
 
-# get the source - not needed because of %{pypi_source} in spec
-# pip download --no-deps --no-binary :all: jc
+# get the source
+cd ~/rpmbuild/SOURCES
+pip download --no-deps --no-binary :all: jc
+cd ~
 
 # build the RPM and SRPM
-rpmbuild -bb -bs ~/rpmbuild/SPEC/python-jc.spec
+rpmbuild -bb ~/rpmbuild/SPECS/python-jc.spec
+rpmbuild -bs ~/rpmbuild/SPECS/python-jc.spec
