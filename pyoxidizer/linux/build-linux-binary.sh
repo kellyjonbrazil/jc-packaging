252bb62e126b5bed7abb6c810c67ad028316e0dc
Kelly Brazil <kellyjonbrazil@gmail.com>
2024-03-18 09:41:12 -0700

    add arm support

--- pyoxidizer/linux/build-linux-binary.sh
+++ pyoxidizer/linux/build-linux-binary.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Linux Build Requirements:
-#     CentOS 7
-
 if [[ $1 == "" ]]; then
     echo "Please enter the version to build."
     exit
@@ -11,8 +8,14 @@ fi
 NAME=jc
 VERSION=$1
 
+if [[ $(uname -m) == "x86_64" ]]; then
+    ARCH=x86_64
+else
+    ARCH=aarch64
+fi
+
 pyoxidizer build --release
 
-cd build/x86_64-unknown-linux-gnu/release/install
-sha256sum "${NAME}" > "${HOME}/dist/${NAME}-${VERSION}-linux-x86_64.sha256"
-tar -czvf "${HOME}/dist/${NAME}-${VERSION}-linux-x86_64.tar.gz" "${NAME}"
+cd "build/${ARCH}-unknown-linux-gnu/release/install"
+sha256sum "${NAME}" > "${HOME}/dist/${NAME}-${VERSION}-linux-${ARCH}.sha256"
+tar -czvf "${HOME}/dist/${NAME}-${VERSION}-linux-${ARCH}.tar.gz" "${NAME}"
