243be571019c7f092dea5e423276622d988512b3
Kelly Brazil <kellyjonbrazil@gmail.com>
2026-09-06 15:54:38 -0700

    add support for Rosetta x86_64 builds on my apple silicon laptop

--- pyoxidizer/darwin/build-osx-binary.sh
+++ pyoxidizer/darwin/build-osx-binary.sh
@@ -9,12 +9,16 @@ NAME=jc
 VERSION=$1
 
 if [[ $(uname -m) == "x86_64" ]]; then
+    # Running under Rosetta on my Apple Silicon Mac
     ARCH=x86_64
+    pyox_binary="${HOME}/.local/bin/pyoxidizer"
+    SDKROOT=$(xcrun --show-sdk-path) $pyox_binary build --target-triple x86_64-apple-darwin --release
+
 else
     ARCH=aarch64
+    pyox_binary="/usr/local/bin/pyoxidizer"
+    $pyox_binary build --release
 fi
-
-pyoxidizer build --release
 
 cd "build/${ARCH}-apple-darwin/release/install"
 shasum -a 256 "${NAME}" > "${HOME}/dist/${NAME}-${VERSION}-darwin-${ARCH}.sha256"
