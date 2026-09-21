abf6cdef5126f1b8d8f5e908035e53192c06c6b3
Kelly Brazil <kellyjonbrazil@gmail.com>
2026-09-21 10:42:43 -0700

    change macos pyoxidizer bin paths for use in build vm

--- pyoxidizer/darwin/build-osx-binary.sh
+++ pyoxidizer/darwin/build-osx-binary.sh
@@ -9,14 +9,16 @@ NAME=jc
 VERSION=$1
 
 if [[ $(uname -m) == "x86_64" ]]; then
-    # Running under Rosetta on my Apple Silicon Mac
+    # Running under Rosetta on Apple Silicon Mac VM
     ARCH=x86_64
-    pyox_binary="${HOME}/.local/bin/pyoxidizer"
+    pyox_binary="${HOME}/pyoxidizer-x86"
+    # pyox_binary="${HOME}/.local/bin/pyoxidizer"
     SDKROOT=$(xcrun --show-sdk-path) $pyox_binary build --target-triple x86_64-apple-darwin --release
 
 else
     ARCH=aarch64
-    pyox_binary="/usr/local/bin/pyoxidizer"
+    pyox_binary="${HOME}/pyoxidizer-arm"
+    # pyox_binary="/usr/local/bin/pyoxidizer"
     $pyox_binary build --release
 fi
 
